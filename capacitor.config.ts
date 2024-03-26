04c903b67b8b7bf1ac03cfc3b3c30e8f67bdf442
Nabullah Ansari <nansari@anviam.com>
2024-03-26 11:29:46 +0530

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'geeksnotes_mobile',
+  appId: 'io.mobile.geeksnotes',
+  appName: 'Geeks Notes',
   webDir: 'www',
   server: {
     androidScheme: 'https'
